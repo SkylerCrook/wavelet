1d7f94ce8400658bb2bbf9a328e93070adc918b3
SkylerCrook <jskycrook@gmail.com>
2023-01-19 16:01:11 -0800

    Update SearchEngine.java

--- SearchEngine.java
+++ SearchEngine.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 import java.net.URI;
 import java.util.HashSet;
 
-class Handler implements URLHandler {
+class Handler2 implements URLHandler {
     // The one bit of state on the server: a number that will be manipulated by
     // various requests.
     int num = 0;
@@ -37,6 +37,6 @@ class SearchEngine {
 
         int port = Integer.parseInt(args[0]);
 
-        Server.start(port, new Handler());
+        Server.start(port, new Handler2());
     }
 }
